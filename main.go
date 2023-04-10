1df84dcf149c468619046e79dd0d6503071b860a
George Tipton <gtip22@gmail.com>
2023-04-10 16:24:39 -0600

    Gin template

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+// CompileDaemon.exe -command="./go-api"
+
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
